d2a1cef64e7cb297b8dc90c5fdfe2d3c7898ce6f
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-09-27 00:58:20 +0400

    added about us route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './modules/about-us/about-us/about-us.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/about-us/about-us-routing.module.ts
+++ src/app/modules/about-us/about-us-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutUsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AboutUsRoutingModule { }
+export class AboutUsRoutingModule {}
