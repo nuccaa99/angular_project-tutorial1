6f8a7066b0890423f020e6452e6039342c73bec9
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-09-26 13:47:16 +0400

    added products

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProductsService } from '../services/products.service';
-import { Products } from 'src/types';
+import { Products, Product } from 'src/types';
+import { ProductComponent } from '../components/product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product, "output")
+  }
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
-        console.log(products.items);
+        this.products = products.items;
       });
   }
 }
